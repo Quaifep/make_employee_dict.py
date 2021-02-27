e4dac920dd2341da9cd183284533ebbb2b1890d2
Paul <quaifep@yahoo.com>
2021-02-26 19:20:46 -0800

    Update to make classes private.

--- make_employee_dict.py
+++ make_employee_dict.py
@@ -4,33 +4,33 @@
 
 class Employee:
     """Employee Class"""
-    def __init__(self, _emp_names, _emp_ids, _emp_salary, _emp_emails):
+    def __init__(self, _names, _ids, _salary, _emails):
         """ init method creating private names, ids, salaries, and emails"""
-        self.emp_names = _emp_names
-        self.emp_ids = _emp_ids
-        self.emp_salary = _emp_salary
-        self.emp_emails = _emp_emails
+        self._names = _names
+        self._ids = _ids
+        self._salary = _salary
+        self._emails = _emails
 
     def get_name(self):
         """returns name"""
-        return self._emp_names
+        return self._names
 
     def get_ID_number(self):
         """returns ID number"""
-        return self._emp_ids
+        return self._ids
 
     def get_salary(self):
         """returns salary"""
-        return self._emp_salary
+        return self._salary
 
     def get_email_address(self):
         """returns email address"""
-        return self._emp_emails
+        return self._emails
 
 
-def make_employee_dict(_emp_names, _emp_ids, _emp_sals, _emp_emails):  # to make the list into employee object
+def make_employee_dict(_names, _ids, _salary, _emails):  # to make the list into employee object
     result = {}
-    for i in range(len(_emp_names)):
-        emp = Employee(_emp_names[i], _emp_ids[i], _emp_sals[i], _emp_emails[i])
-        result[_emp_ids[i]] = emp
+    for i in range(len(_names)):
+        emp = Employee(_names[i], _ids[i], _salary[i], _emails[i])
+        result[_ids[i]] = emp
     return result
