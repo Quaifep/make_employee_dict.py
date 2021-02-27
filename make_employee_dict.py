a9f556215fed572a4e1898f7e1be2b347680c6de
Paul <quaifep@yahoo.com>
2021-02-26 19:37:56 -0800

    Made Exact

--- make_employee_dict.py
+++ make_employee_dict.py
@@ -4,12 +4,12 @@
 
 class Employee:
     """Employee Class"""
-    def __init__(self, _name, _ID_number, _salary, _emails):
+    def __init__(self, _name, _ID_number, _salary, _email_address):
         """ init method creating private names, ids, salaries, and emails"""
         self._name = _name
         self._ID_number = _ID_number
         self._salary = _salary
-        self._emails = _emails
+        self._email_address = _email_address
 
     def get_name(self):
         """returns name"""
@@ -25,12 +25,12 @@ class Employee:
 
     def get_email_address(self):
         """returns email address"""
-        return self._emails
+        return self._email_address
 
 
-def make_employee_dict(_name, _ID_number, _salary, _emails):
+def make_employee_dict(_name, _ID_number, _salary, _email_address):
     result = {}
     for i in range(len(_name)):
-        emp = Employee(_name[i], _ID_number[i], _salary[i], _emails[i])
+        emp = Employee(_name[i], _ID_number[i], _salary[i], _email_address[i])
         result[_ID_number[i]] = emp
     return result
